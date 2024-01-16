7771ca108a27f4e5beea50b0d9e60a464893bab9
speechless42 <94534629+speechless42@users.noreply.github.com>
2024-01-17 01:35:22 +0800

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
+// const AutoImport = require('unplugin-auto-import/webpack')
+// const Component = require('unplugin-vue-import/webpack')
+// const {ElementPlusResolver} = require('unplugin-vue-import/resolvers')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // configureWebpack:{
+  //   plugins:[
+  //     AutoImport({resolvers:[ElementPlusResolver()]}),
+  //     Component({resolvers:[ElementPlusResolver()]})
+  //   ]
+  // }
 })
